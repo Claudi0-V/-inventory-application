35ec67234bd694d2213245fb8e96f64782a2250e
Claudi0-V <claudiov.dejesus@protonmail.com>
2021-12-08 15:47:47 -0300

    create models/schemes for categories and items

--- models/categories.js
+++ models/categories.js
@@ -1,2 +1,25 @@
 // nane description url
-//
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const CategoryScheme = new Schema({
+    name: {
+        type: String,
+        min: 5,
+        max: 32,
+        required: true,
+    },
+    description: {
+        type: String,
+        min: 5,
+        max: 125,
+        required: true,
+    },
+    url: {
+        type: String,
+        required: true,
+    }
+})
+
+const Category = mongoose.model('Category', CategoryScheme)
+module.exports = Category;
--- models/items.js
+++ models/items.js
@@ -1,1 +1,35 @@
-// name, description, category priice, number in stock
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ItemSchema = new Schema({
+    name: {
+        type: String,
+        min: 3,
+        max: 50,
+        required: true,
+    },
+    description: {
+        type: String,
+        min: 10,
+        max: 150,
+        required: true,
+    },
+    category: {
+        type: String,
+        min: 3,
+        max: 20,
+        required: true,
+    },
+    price: {
+        type: Number,
+        min: 0005,
+        required: true
+    },
+    stock_quantity: {
+        type: Number,
+        required: true
+    }
+})
+
+const Item = mongoose.model('Item', ItemSchema)
+module.exports = Item;
