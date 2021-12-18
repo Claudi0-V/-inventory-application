9c09afba502b090f15b7236ef724025ec2de5d0e
Claudi0-V <claudiov.dejesus@protonmail.com>
2021-12-18 19:55:25 -0300

    Fixed the page to accept validation and catch errors

--- models/categories.js
+++ models/categories.js
@@ -1,20 +1,20 @@
-const mongoose = require('mongoose');
-const { Schema } = mongoose
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
 
 const CategoryScheme = new Schema({
-    name: {
-        type: String,
-        min: 5,
-        max: 32,
-        required: true,
-    },
-    description: {
-        type: String,
-        min: 5,
-        max: 125,
-        required: true,
-    },
-})
+  name: {
+    type: String,
+    min: 5,
+    max: 32,
+    required: true,
+  },
+  description: {
+    type: String,
+    min: 5,
+    max: 500,
+    required: true,
+  },
+});
 
-const Category = mongoose.model('Category', CategoryScheme)
+const Category = mongoose.model("Category", CategoryScheme);
 module.exports = Category;
--- routes/categories.js
+++ routes/categories.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const Category = require("../models/categories.js");
+const { body, validationResult } = require("express-validator");
 
 router.get("/", async (req, res) => {
   try {
     const allCategories = await Category.find();
-    console.log(allCategories);
     res.render("categories_category", {
       title: "All Categories",
       allCategories,
@@ -23,18 +23,34 @@ router.get("/new-category", (req, res) => {
   }
 });
 
-router.post("/", async (req, res) => {
-  try {
-    const item = new Category(req.body);
-    await item.save();
-    res.redirect("/categories");
-  } catch (err) {
-    console.log(err);
+router.post(
+  "/",
+  [
+    body("name").isLength({ min: 5, max: 32 }).escape(),
+    body("description").isLength({ min: 5, max: 500}).escape(),
+  ],
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        res.render("categories_new-category", {
+          title: "new category",
+          error: req.body,
+        });
+      } else {
+        const category = new Category(req.body);
+        await category.save();
+        res.redirect("/categories");
+      }
+    } catch (err) {
+      console.log(err);
+    }
   }
-});
+);
 
 router.get("/:category", async (req, res) => {
-  const category = await Item.find({ req.params.category });
+  const param = req.params.category;
+  const category = await Category.find({ param });
   res.render("categories_specific-category", { category });
 });
 
