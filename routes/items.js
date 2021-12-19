619b940993476e2d417e20d7d0df51191421a29a
Claudi0-V <claudiov.dejesus@protonmail.com>
2021-12-19 18:54:35 -0300

    finished the item CRUD routes

--- routes/items.js
+++ routes/items.js
@@ -1,24 +1,25 @@
 const express = require("express");
 const router = express.Router();
 const Item = require("../models/items.js");
+const Category = require("../models/categories.js")
 const { body, validationResult } = require("express-validator");
-
-const fakeItems = [
-  {
-    name: "pencil",
-    description: "small pencil",
-    category: "school",
-    price: 199,
-    stock_quantity: 25,
-  },
-];
 
 router.get("/", async (req, res) => {
   try {
-    const items = await Item.find();
+    const items = await Item.find().populate('category');
     res.render("items_all-items", { title: "inventory", items });
   } catch (error) {
     console.log(error);
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const wat =  await Item.findByIdAndDelete(id);
+    res.json({ redirect: "/items" });
+  } catch (err) {
+    console.log(err);
   }
 });
 
@@ -50,17 +51,65 @@ router.post(
   }
 );
 
-router.get("/new-item", (req, res) => {
-  res.render("items_new-item", { title: "Add New Item" });
+router.get("/new-item", async (req, res) => {
+  const categories = await Category.find();
+  res.render("items_new-item", { title: "Add New Item", categories });
 });
 
-router.get("/:item", async (req, res) => {
-  const itemParam = req.params.item;
-  const item = await Item.find({ itemParam });
+router.get("/:id", async (req, res) => {
+  const id = req.params.id;
+  const item = await Item.findById(id).populate("category");
   res.render("items_specific-item", {
     title: item.name,
     item,
   });
 });
+
+
+
+
+
+router.get("/:id/update", async (req, res) => {
+  const id = req.params.id;
+  const item = await Item.findById(id).populate("category");
+  const categories = await Category.find();
+
+  res.render("items_update-item", {
+    title: "Update Item",
+    item,
+    categories
+  });
+});
+
+router.post(
+  "/:id/update",
+  [
+    body("name").isLength({ min: 3, max: 50 }).escape(),
+    body("description").isLength({ min: 10, max: 150 }).escape(),
+    body("category").isLength({ min: 5, max: 32 }).escape(),
+    body("price").isInt().escape(),
+    body("stock_quantity").isInt().escape(),
+  ],
+  async (req, res) => {
+    try {
+      const id = req.params.id;
+      console.log(id)
+      const errors = validationResult(req);
+      if (!errors) {
+        res.render("items_update-item", {
+          title: "Update Item",
+          error: req.body,
+        });
+      } else {
+        const item = Item.findByIdAndUpdate(id, req.body, () => console.log("id:", id))
+        res.redirect("/items");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
+
+
 
 module.exports = router;
